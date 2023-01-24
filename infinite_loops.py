6ddad25af78106ce81fb82952e9baab1300dd5a4
ARBUCHELI <radio_seattle@hotmail.com>
2023-01-23 21:22:06 -0500

    adding infinite_loops and loop_control_break

---
+++ infinite_loops.py
@@ -0,0 +1,8 @@
+students_period_A = ["Alex", "Briana", "Cheri", "Daniele"]
+students_period_B = ["Dora", "Minerva", "Alexa", "Obie"]
+
+for student in students_period_A:
+  students_period_B.append(student)
+  #print(student)
+  students_period_B.sort()
+print(students_period_B)
